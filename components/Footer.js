ac40796ac65fb747a04e48882edf2baa45b4b126
laxmariappan <elaxmariappan@gmail.com>
2022-01-02 21:29:36 +0530

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => (
+  <div className="Footer border-t bg-white mt-5">
+    <div className="container mx-auto">
+      <div className="p-4 ">
+        <p className=" text-sm text-center text-black "> © 2022 </p>
+      </div>
+    </div>
+  </div>
+);
+export default Footer;
