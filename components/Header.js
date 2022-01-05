9b8e96699d81a64b02e4110d8a18258d2b5e4aff
laxmariappan <elaxmariappan@gmail.com>
2022-01-05 20:19:45 +0530

    1 file modified

--- components/Header.js
+++ components/Header.js
@@ -38,7 +38,7 @@ const Header = () => {
       <nav className="flex flex-wrap items-center justify-between p-4 ">
         <div className="lg:order-2 w-auto lg:w-1/5 lg:text-center">
           <a
-            className="text-xl text-indigo-600 font-semibold font-heading"
+            className="text-xl text-indigo-600 dark:text-white font-semibold font-heading"
             href="#"
             data-config-id="brand"
           >
