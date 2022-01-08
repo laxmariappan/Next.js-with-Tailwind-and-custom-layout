c5ffd5f1820354ce5f4b5175ed260d18e3dac22c
laxmariappan <elaxmariappan@gmail.com>
2022-01-08 13:22:41 +0530

    2 files modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
 
-import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
   )
 }
 
