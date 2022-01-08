7766be7d93085b0027b5e0f6551aeb9f65626b77
laxmariappan <elaxmariappan@gmail.com>
2022-01-08 13:09:31 +0530

    1 file modified

--- pages/about.js
+++ pages/about.js
@@ -6,18 +6,10 @@ const About = () => {
       <Layout>
         <section className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-blue-50">
           <h1 className="text-6xl font-bold">
-            Welcome to{' '}
-            <a className="text-blue-600" href="https://nextjs.org">
-              Next.js!
-            </a>
+            About
           </h1>
 
-          <p className="mt-3 text-2xl ">
-            Get started by editing{' '}
-            <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-              pages/index.js
-            </code>
-          </p>
+          
 
           <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full ">
             <a
